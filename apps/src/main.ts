8a3d28df2e2649cb474715082bb952bedd2ad5c0
Renato Douglas Zembrani <renato.zembrani@hotmail.com>
2025-11-27 17:35:12 -0300

    Swagger : remove swagger - use postman instead

--- apps/src/main.ts
+++ apps/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
@@ -14,15 +13,6 @@ app.useLogger(app.get(Logger));
       transform: true,
     })
   );
-
-  const config = new DocumentBuilder()
-    .setTitle('Brain Agriculture API')
-    .setDescription('Brain Agriculture API description')
-    .setVersion('1.0')
-    .addTag('Brain Agriculture')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
