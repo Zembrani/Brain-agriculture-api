9bf702f5fb2885a5f1a38bb2b1febc1597200a9a
Renato Douglas Zembrani <renato.zembrani@hotmail.com>
2025-11-27 16:17:54 -0300

    Security : add env file to hide keys

--- apps/src/app.module.ts
+++ apps/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { ProducerModule } from "./modules/producer.module";
 import { FarmModule } from "./modules/farm.module";
 import { CropModule } from "./modules/crop.module";
@@ -9,19 +10,36 @@ import { DashboardModule } from "./modules/dashboard.module";
   controllers: [],
   providers: [],
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
+      ignoreEnvFile: process.env.NODE_ENV === "test",
+    }),
     ProducerModule,
     FarmModule,
     CropModule,
     DashboardModule,
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "pguser",
-      password: "secret123",
-      database: process.env.POSTGRES_DATABASE || "agriculture",
-      synchronize: false,
-      autoLoadEntities: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const isTestEnvironment = configService.get("NODE_ENV") === "test";
+
+        return {
+          type: "postgres",
+          host: configService.get<string>("POSTGRES_HOST"),
+          port: isTestEnvironment
+            ? configService.get<number>("POSTGRES_PORT")
+            : configService.get<number>("POSTGRES_PORT"),
+          username: configService.get<string>("POSTGRES_USER"),
+          password: configService.get<string>("POSTGRES_PASSWORD"),
+          database: isTestEnvironment
+            ? configService.get<string>("POSTGRES_DATABASE")
+            : configService.get<string>("POSTGRES_DATABASE"),
+          synchronize: false,
+          autoLoadEntities: true,
+        };
+      },
     }),
   ],
 })
