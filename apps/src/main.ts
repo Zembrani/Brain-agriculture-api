cbb9b57d96c87dda0bf87fa233e0f1737a54df4c
Renato Douglas Zembrani <renato.zembrani@hotmail.com>
2025-11-27 16:44:18 -0300

    Documentation : add Swagger

--- apps/src/main.ts
+++ apps/src/main.ts
@@ -1,16 +1,28 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log'],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
-    }),
+    })
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Brain Agriculture API')
+    .setDescription('Brain Agriculture API description')
+    .setVersion('1.0')
+    .addTag('Brain Agriculture')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((error) => {
